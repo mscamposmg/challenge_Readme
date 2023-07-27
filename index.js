a01339f5c5ef25f4752c3d2675f895d025458669
mscamposmg <mscamposmg@gmail.com>
2023-07-27 14:16:32 +1000

    teste

--- index.js
+++ index.js
@@ -2,61 +2,61 @@
 // ***All packages was included already, such as ".gitignore (node_modules/ and .DS_Store/)", "npm init", "npm install", "npm install inquirer@8.2.4".***
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
-        {type: 'input',
-        name: 'author',
-        message: 'Write your name:',
+        {type: "input",
+        name: "author",
+        message: "Write your name:"
         },
-        {type: 'input',
-        name: 'name',
-        message: 'What is the name of your Project?',
+        {type: "input",
+        name: "name",
+        message: "What is the name of your Project?",
         },
-        {type: 'input',
-        name: 'description',
-        message: 'Describe what does your Project do:',
+        {type: "input",
+        name: "description",
+        message: "Describe what does your Project do:"
         },
-        {type: 'input',
-        name: 'installation',
-        message: 'Describe how to install your Project:',
+        {type: "input",
+        name: "installation",
+        message: "Describe how to install your Project:"
         },
-        {type: 'input',
-        name: 'usage',
-        message: 'Describe how your Project works',
+        {type: "input",
+        name: "usage",
+        message: "Describe how your Project works"
         },
-        {type: 'input',
-        name: 'contributing',
-        message: 'What is the contribuition for your Project?',
+        {type: "input",
+        name: "contributing",
+        message: "What is the contribuition for your Project?"
         },
-        {type: 'input',
-        name: 'test',
-        message: 'What are the tests instructions of your Project?',
+        {type: "input",
+        name: "test",
+        message: "What are the tests instructions of your Project?",
         },
-        {type: 'list',
-        name: 'license',
+        {type: "list",
+        name: "license",
         choices:
             [
                 "GNU",
                 "ISC",
                 "MIT",
             ],
-        message: 'Choose the right license for your Project?',
+        message: "Choose the right license for your Project?"
         },
-        {type: 'input',
-        name: 'github',
-        message: 'ENTER your GitHub Username:',
+        {type: "input",
+        name: "github",
+        message: "ENTER your GitHub Username:"
         },
-        {type: 'input',
-        name: 'email',
-        message: 'ENTER your email:',
+        {type: "input",
+        name: "email",
+        message: "ENTER your email:"
         },
     ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile("template_README.md", JSON.stringify(data, null, '\t'), (err) =>
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
     err ? console.log(err) : console.log('success!')
     );
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
-const inquirer = require("inquirer");
-const fs = require('fs');
+// const inquirer = require("inquirer");
+// const fs = require('fs');
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
@@ -38,8 +38,8 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  ${renderLicenseBadge(data.license)}
+  return `# ${data.name}
+  ${renderLicenseBadge(data.licenses)}
   
   ## Description
   ${data.description}
